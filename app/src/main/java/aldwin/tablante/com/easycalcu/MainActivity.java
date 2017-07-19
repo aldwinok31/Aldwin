b5513c0e2fb653cbe53310d9e60ca81cd92fe5ed
Aldwin <aldwinotablante@gmail.com>
2017-07-19 18:59:18 +0800

    Initial Project Structure

--- app/src/main/java/aldwin/tablante/com/easycalcu/MainActivity.java
+++ app/src/main/java/aldwin/tablante/com/easycalcu/MainActivity.java
@@ -6,8 +6,9 @@ import android.widget.EditText;
 import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
-
-
+private int finalAnswer=0;
+private int numF=0;
+    private int numS=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +19,61 @@ public class MainActivity extends AppCompatActivity {
 EditText answer = (EditText) findViewById(R.id.editText3);
         EditText firstNum = (EditText) findViewById(R.id.editText);
         EditText secondNum = (EditText) findViewById(R.id.editText2);
-        int numF = Integer.parseInt(firstNum.getText().toString());
-        int numS =Integer.parseInt(secondNum.getText().toString());
+        numF = Integer.parseInt(firstNum.getText().toString());
+        numS =Integer.parseInt(secondNum.getText().toString());
         numF = numF - numS;
         String finAl=String.valueOf(numF);
 answer.setText(finAl);
     }
+
+
+    public void doDivide(View view){
+        EditText answer = (EditText) findViewById(R.id.editText3);
+        EditText firstNum = (EditText) findViewById(R.id.editText);
+        EditText secondNum = (EditText) findViewById(R.id.editText2);
+        numF = Integer.parseInt(firstNum.getText().toString());
+        numS =Integer.parseInt(secondNum.getText().toString());
+        finalAnswer = numF / numS;
+        String finAl=String.valueOf(finalAnswer);
+        answer.setText(finAl);
+
+    }
+    public void doPlus(View view){
+        EditText answer = (EditText) findViewById(R.id.editText3);
+        EditText firstNum = (EditText) findViewById(R.id.editText);
+        EditText secondNum = (EditText) findViewById(R.id.editText2);
+        numF = Integer.parseInt(firstNum.getText().toString());
+        numS =Integer.parseInt(secondNum.getText().toString());
+        finalAnswer = numF + numS;
+        String finAl=String.valueOf(finalAnswer);
+        answer.setText(finAl);
+
+    }
+    public void doProduct(View view){
+        EditText answer = (EditText) findViewById(R.id.editText3);
+        EditText firstNum = (EditText) findViewById(R.id.editText);
+        EditText secondNum = (EditText) findViewById(R.id.editText2);
+        numF = Integer.parseInt(firstNum.getText().toString());
+        numS =Integer.parseInt(secondNum.getText().toString());
+        finalAnswer = numF * numS;
+        String finAl=String.valueOf(finalAnswer);
+        answer.setText(finAl);
+
+    }
+    public void doExit(View view){
+        finish();
+
+    }
+    public void doClear(View view){
+        EditText answer = (EditText) findViewById(R.id.editText3);
+        EditText firstNum = (EditText) findViewById(R.id.editText);
+        EditText secondNum = (EditText) findViewById(R.id.editText2);
+        answer.setText("");
+        firstNum.setText("");
+        secondNum.setText("");
+        answer.setHint("0");
+        firstNum.setHint("Enter First Number");
+        secondNum.setHint("Enter Second Number");
+    }
+
 }
